fe8f5a89b7787b721d53dad99e4c67e434800386
Arindam Chakraborty <arismart99@gmail.com>
2017-09-21 17:10:18 +0530

    adds the first and simple validation to item model. Breaking as of now

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,25 +1,25 @@
 class Item < ApplicationRecord
   ALLOWED_TYPES_OF_ITEMS = %w(ARMOR CRAFTABLE FOOD WEAPON MEDICINE RAW_MATERIAL PROCESSED_MATERIAL DROPPED_ITEM FURNITURE TOOLS USER_DEFINED)
-  validates_presence_of :item_type
-  validates_inclusion_of :item_type, :in => ALLOWED_TYPES_OF_ITEMS
-
   before_validation :infer_default_values_from_item_type
 
+  validates_presence_of :item_type
+  validates_inclusion_of :item_type, :in => ALLOWED_TYPES_OF_ITEMS
   validates_presence_of :name
 
   # Validations to ensure there are references to the actual items
-  validate :must_have_weapon, :if => :weapon?
-  validate :must_have_recipe, :if => :craftable?
-  validate :must_have_raw_material, :if => :raw_material?
-  validate :must_have_processed_material, :if => :processed_material?
-  validate :must_have_armor, :if => :armor?
-  validate :must_have_food, :if => :food?
-  validate :must_have_medicine, :if => :medicine?
+  validate :must_have_weapon
+  # validate :must_have_recipe, :if => :recipe?
+  # validate :must_have_raw_material, :if => :raw_material?
+  # validate :must_have_processed_material, :if => :processed_material?
+  # validate :must_have_armor, :if => :armor?
+  # validate :must_have_food, :if => :food?
+  # validate :must_have_medicine, :if => :medicine?
 
 
   private
 
   def infer_default_values_from_item_type
+    Rails.logger.info "\n\n\nBefore assigning value, type= #{item_type}, weapon= #{weapon}\n\n\n"
     case item_type
     when 'ARMOR'
       armor = true
@@ -44,9 +44,11 @@ class Item < ApplicationRecord
     when 'USER_DEFINED'
       user_defined = true
     end
+    Rails.logger.info "\n\n\nAfter assigning VALUE, type= #{item_type}, weapon= #{weapon}\n\n\n"
   end
 
   def must_have_weapon
-    errors.add(:item_type, :must_have_weapon) unless weapon_id.present?
+    Rails.logger.info "\n\n\nInside must have weapon, weapon= #{weapon}, weapon_id= #{weapon_id}\n\n\n"
+    errors.add(:item_type, :must_have_weapon) if weapon? && weapon_id.blank?
   end
 end
