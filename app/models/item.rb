fc3689bd25e627f0e7e24d58f7d87f681128ae26
Arindam Chakraborty <arismart99@gmail.com>
2017-09-21 15:21:02 +0530

    complete the list of item types, process the new ones, add validation skeleton

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-  ALLOWED_TYPES_OF_ITEMS = %w(ARMOR FOOD WEAPON MEDICINE RAW_MATERIAL DROPPED_ITEM FURNITURE TOOLS TRANSPORT USER_DEFINED)
+  ALLOWED_TYPES_OF_ITEMS = %w(ARMOR CRAFTABLE FOOD WEAPON MEDICINE RAW_MATERIAL PROCESSED_MATERIAL DROPPED_ITEM FURNITURE TOOLS TRANSPORT USER_DEFINED)
   validate_presence_of :item_type
   validate_inclusion_of :item_type, :in => ALLOWED_TYPES_OF_ITEMS
 
@@ -7,20 +7,48 @@ class Item < ApplicationRecord
 
   validate_presence_of :name
 
+  # Validations to ensure there are references to the actual items
+  validate :must_have_weapon, :if => :weapon?
+  validate :must_have_recipe, :if => :craftable?
+  validate :must_have_raw_material, :if => :raw_material?
+  validate :must_have_processed_material, :if => :processed_material?
+  validate :must_have_armor, :if => armor?
+  validate :must_have_food, :if => food?
+  validate :must_have_medicine, :if => :medicine?
+
+
   private
 
   def infer_default_values_from_item_type
     case item_type
+    when 'ARMOR'
+      armor = true
+    when 'CRAFTABLE'
+      craftable = true
     when 'FOOD'
-      usable_as_food = true
+      food = true
     when 'WEAPON'
-      usable_as_weapon = true
+      weapon = true
     when 'MEDICINE'
-      usable_as_heal = true
+      medicine = true
     when 'RAW_MATERIAL'
-      unedible_raw_material = true
+      raw_material = true
+    when 'PROCESSED_MATERIAL'
+      processed_material = true
     when 'DROPPED_ITEM'
-      dropped_in_the_game = true
+      drop_item = true
+    when 'FURNITURE'
+      furniture = true
+    when 'TOOLS'
+      tool = true
+    when 'TRANSPORT'
+      transport = true
+    when 'USER_DEFINED'
+      user_defined = true
     end
+  end
+
+  def must_have_weapon
+    errors.add(:item_type, :must_have_weapon) unless weapon_id.present?
   end
 end
